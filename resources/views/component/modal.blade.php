f4738e15983e30f2cfbfa898725439ea41f39a48
Prima Nugraha <dotprima@hotmail.com>
2022-10-26 22:52:22 +0700

    first commit

--- resources/views/component/modal.blade.php
+++ resources/views/component/modal.blade.php
@@ -182,63 +182,58 @@
     <div class="offcanvas-body">
         <div id="offcanvasNav" class="offcanvas-menu-nav">
             <ul>
-                <li class="offcanvas-nav-parent"><a class="offcanvas-nav-item" href="#">home</a>
-                    <ul>
-                        <li><a href="index-2.html">Home One</a></li>
-                        <li><a href="index-two.html">Home Two</a></li>
-                    </ul>
-                </li>
-                <li class="offcanvas-nav-parent"><a class="offcanvas-nav-item" href="about-us.html">about</a>
-                </li>
-                <li class="offcanvas-nav-parent"><a class="offcanvas-nav-item" href="#">shop</a>
-                    <ul>
-                        <li><a href="#" class="offcanvas-nav-item">Shop Layout</a>
-                            <ul>
-                                <li><a href="product.html">Shop 3 Column</a></li>
-                                <li><a href="product-four-columns.html">Shop 4 Column</a></li>
-                                <li><a href="product-left-sidebar.html">Shop Left Sidebar</a></li>
-                                <li><a href="product-right-sidebar.html">Shop Right Sidebar</a></li>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">about</a></li>
+                <!-- <li class="has-submenu position-static"><a href="product.html">shop</a>
+                            <ul class="submenu-nav-mega">
+                                <li><a href="#/" class="mega-title">Shop Layout</a>
+                                    <ul>
+                                        <li><a href="product.html">Shop 3 Column</a></li>
+                                        <li><a href="product-four-columns.html">Shop 4 Column</a></li>
+                                        <li><a href="product-left-sidebar.html">Shop Left Sidebar</a></li>
+                                        <li><a href="product-right-sidebar.html">Shop Right Sidebar</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#/" class="mega-title">Single Product</a>
+                                    <ul>
+                                        <li><a href="product-details-normal.html">Single Product Normal</a></li>
+                                        <li><a href="product-details.html">Single Product Variable</a></li>
+                                        <li><a href="product-details-group.html">Single Product Group</a></li>
+                                        <li><a href="product-details-affiliate.html">Single Product
+                                                Affiliate</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#/" class="mega-title">Others Pages</a>
+                                    <ul>
+                                        <li><a href="product-cart.html">Shopping Cart</a></li>
+                                        <li><a href="product-checkout.html">Checkout</a></li>
+                                        <li><a href="product-wishlist.html">Wishlist</a></li>
+                                        <li><a href="product-compare.html">Compare</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li> -->
+                <!-- <li class="has-submenu"><a href="blog.html">Blog</a>
+                            <ul class="submenu-nav">
+                                <li class="has-submenu"><a href="#/">Blog Layout</a>
+                                    <ul class="submenu-nav">
+                                        <li><a href="blog.html">Blog Grid</a></li>
+                                        <li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
+                                        <li><a href="blog-right-sidebar.html">Blog Right Sidebar</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="blog-details.html">Blog Details</a></li>
                             </ul>
                         </li>
-                        <li><a href="#" class="offcanvas-nav-item">Single Product</a>
-                            <ul>
-                                <li><a href="product-details-normal.html">Single Product Normal</a></li>
-                                <li><a href="product-details.html">Single Product Variable</a></li>
-                                <li><a href="product-details-group.html">Single Product Group</a></li>
-                                <li><a href="product-details-affiliate.html">Single Product Affiliate</a></li>
+                        <li class="has-submenu"><a href="account-login.html">Pages</a>
+                            <ul class="submenu-nav">
+                                <li><a href="account-login.html">My Account</a></li>
+                                <li><a href="faq.html">Frequently Questions</a></li>
+                                <li><a href="page-not-found.html">Page Not Found</a></li>
                             </ul>
-                        </li>
-                        <li><a href="#" class="offcanvas-nav-item">Others Pages</a>
-                            <ul>
-                                <li><a href="product-cart.html">Shopping Cart</a></li>
-                                <li><a href="product-checkout.html">Checkout</a></li>
-                                <li><a href="product-wishlist.html">Wishlist</a></li>
-                                <li><a href="product-compare.html">Compare</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-                <li class="offcanvas-nav-parent"><a class="offcanvas-nav-item" href="#">Blog</a>
-                    <ul>
-                        <li><a class="offcanvas-nav-item" href="#">Blog Layout</a>
-                            <ul>
-                                <li><a href="blog.html">Blog Grid</a></li>
-                                <li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
-                                <li><a href="blog-right-sidebar.html">Blog Right Sidebar</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="blog-details.html">Blog Details</a></li>
-                    </ul>
-                </li>
-                <li class="offcanvas-nav-parent"><a class="offcanvas-nav-item" href="#">Pages</a>
-                    <ul>
-                        <li><a href="account-login.html">My Account</a></li>
-                        <li><a href="faq.html">Frequently Questions</a></li>
-                        <li><a href="page-not-found.html">Page Not Found</a></li>
-                    </ul>
-                </li>
-                <li class="offcanvas-nav-parent"><a class="offcanvas-nav-item" href="contact.html">Contact</a>
-                </li>
+                        </li> -->
+                <li><a href="/contact">Contact</a></li>
+                <li><a href="/catalog">Product</a></li>
             </ul>
         </div>
     </div>
