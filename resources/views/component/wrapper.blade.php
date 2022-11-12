dc8a58d578450558c425f8699ddd3b982530e00e
prima <dotprima@hotmail.com>
2022-11-13 00:24:56 +0700

    'update'

--- resources/views/component/wrapper.blade.php
+++ resources/views/component/wrapper.blade.php
@@ -8,12 +8,11 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="description" content="Diarika">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="keywords"
-        content="Diarika Beauty Glow up your skin. Find your truly beauty" />
+    <meta name="keywords" content="Diarika Beauty Glow up your skin. Find your truly beauty" />
     <meta name="author" content="codecarnival" />
 
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{env('APP_URL')}}/assets/images/favicon.webp">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ env('APP_URL') }}/assets/images/favicon.webp">
 
     <!-- CSS (Font, Vendor, Icon, Plugins & Style CSS files) -->
 
@@ -23,14 +22,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&amp;display=swap"
         rel="stylesheet">
 
-    <link rel="canonical" href="{{env('APP_URL')}}" />
+    <link rel="canonical" href="{{ env('APP_URL') }}" />
     <meta property="og:locale" content="id_ID" />
     <meta property="og:type" content="website" />
-   
-    <meta property="og:description"
-        content="Diarika Beauty - Glow up your skin. Find your truly beauty" />
-    <meta property="og:url" content="{{env('APP_URL')}}" />
- 
+
+    <meta property="og:description" content="Diarika Beauty - Glow up your skin. Find your truly beauty" />
+    <meta property="og:url" content="{{ env('APP_URL') }}" />
+
     <meta property="article:modified_time" content="2022-08-18T01:56:38+00:00" />
 
     <meta name="twitter:card" content="summary_large_image" />
@@ -38,19 +36,37 @@
     <meta name="twitter:data1" content="7 menit" />
     @yield('css')
     <!-- Vendor CSS (Bootstrap & Icon Font) -->
-    <link rel="stylesheet" href="{{env('APP_URL')}}/assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/assets/css/vendor/bootstrap.min.css">
 
     <!-- Plugins CSS (All Plugins Files) -->
-    <link rel="stylesheet" href="{{env('APP_URL')}}/assets/css/plugins/swiper-bundle.min.css">
-    <link rel="stylesheet" href="{{env('APP_URL')}}/assets/css/plugins/font-awesome.min.css">
-    <link rel="stylesheet" href="{{env('APP_URL')}}/assets/css/plugins/fancybox.min.css">
-    <link rel="stylesheet" href="{{env('APP_URL')}}/assets/css/plugins/range-slider.css">
-    <link rel="stylesheet" href="{{env('APP_URL')}}/assets/css/plugins/nice-select.css">
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/assets/css/plugins/swiper-bundle.min.css">
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/assets/css/plugins/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/assets/css/plugins/fancybox.min.css">
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/assets/css/plugins/range-slider.css">
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/assets/css/plugins/nice-select.css">
     @livewireStyles
     <!-- Style CSS -->
-    <link rel="stylesheet" href="{{env('APP_URL')}}/assets/css/style.min.css">
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/assets/css/style.min.css">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-V38GPWK02H"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
 
+        gtag('config', 'G-V38GPWK02H');
+    </script>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5QTCDVR');</script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body>
@@ -78,22 +94,26 @@
 
     <!-- JS Vendor, Plugins & Activation Script Files -->
 
-    <script src="{{env('APP_URL')}}/assets/js/vendor/modernizr-3.11.7.min.js"></script>
-    <script src="{{env('APP_URL')}}/assets/js/vendor/jquery-3.6.0.min.js"></script>
-    <script src="{{env('APP_URL')}}/assets/js/vendor/jquery-migrate-3.3.2.min.js"></script>
-    <script src="{{env('APP_URL')}}/assets/js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="{{ env('APP_URL') }}/assets/js/vendor/modernizr-3.11.7.min.js"></script>
+    <script src="{{ env('APP_URL') }}/assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="{{ env('APP_URL') }}/assets/js/vendor/jquery-migrate-3.3.2.min.js"></script>
+    <script src="{{ env('APP_URL') }}/assets/js/vendor/bootstrap.bundle.min.js"></script>
 
     <!-- Plugins JS -->
-    <script src="{{env('APP_URL')}}/assets/js/plugins/swiper-bundle.min.js"></script>
-    <script src="{{env('APP_URL')}}/assets/js/plugins/fancybox.min.js"></script>
-    <script src="{{env('APP_URL')}}/assets/js/plugins/range-slider.js"></script>
-    <script src="{{env('APP_URL')}}/assets/js/plugins/jquery.nice-select.min.js"></script>
+    <script src="{{ env('APP_URL') }}/assets/js/plugins/swiper-bundle.min.js"></script>
+    <script src="{{ env('APP_URL') }}/assets/js/plugins/fancybox.min.js"></script>
+    <script src="{{ env('APP_URL') }}/assets/js/plugins/range-slider.js"></script>
+    <script src="{{ env('APP_URL') }}/assets/js/plugins/jquery.nice-select.min.js"></script>
 
     <!-- Custom Main JS -->
-    <script src="{{env('APP_URL')}}/assets/js/main.js"></script>
+    <script src="{{ env('APP_URL') }}/assets/js/main.js"></script>
     @livewireScripts
     @yield('js')
     {{ \TawkTo::widgetCode() }}
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5QTCDVR"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 </body>
 
 
