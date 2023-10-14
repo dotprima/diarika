a114436c91d7cd76d0e49a5d2e0a73ced56f3962
Prima Nugraha <dotprima@hotmail.com>
2023-10-14 17:43:44 +0700

    update

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -6,8 +6,9 @@
                 <div class="col-md-6 col-lg-4">
                     <div class="widget-item">
                         <div class="widget-about">
-                            <a class="widget-logo" href="{{env('APP_URL')}}/">
-                                <img src="{{env('APP_URL')}}/assets/images/logo.PNG" width="95" height="68" alt="Diarika Beauty Official Website">
+                            <a class="widget-logo" href="{{ env('APP_URL') }}/">
+                                <img src="{{ env('APP_URL') }}/assets/images/logo.PNG" width="95" height="68"
+                                    alt="Diarika Beauty Official Website">
                             </a>
                             <p class="desc">Glow up your skin & find your truly beauty</p>
                         </div>
@@ -17,15 +18,21 @@
                     <div class="widget-item">
                         <h4 class="widget-title">Information</h4>
                         <ul class="widget-nav">
-                            <li><a href="https://wa.me/6281395121992">Whatsapp 1 <img src="/icon/whatsapp.png" alt="Diarika Beauty Official Website" width="17px"></a>
+                            <li><a href="https://wa.me/6281395121992">Whatsapp 1 <img src="/icon/whatsapp.png"
+                                        alt="Diarika Beauty Official Website" width="17px"></a>
                             </li>
-                            <li><a href="https://wa.me/6281395112011">Whatsapp 2 <img src="/icon/whatsapp.png" alt="Diarika Beauty Official Website" width="17px"></a>
+                            <li><a href="https://wa.me/6281395112011">Whatsapp 2 <img src="/icon/whatsapp.png"
+                                        alt="Diarika Beauty Official Website" width="17px"></a>
                             </li>
-                            <li><a href="https://shopee.co.id/diarika_beauty_official">Shopee <img src="/icon/shopee.png" alt="Diarika Beauty Official Website" width="30px"></a>
+                            <li><a href="https://shopee.co.id/diarika_beauty_official">Shopee <img
+                                        src="/icon/shopee.png" alt="Diarika Beauty Official Website" width="30px"></a>
                             </li>
-                            <li><a href="https://www.lazada.co.id/shop/diarika-beauty-office">Lazada <img src="/icon/lazada.png" alt="Diarika Beauty Official Website" width="30px"></a>
+                            <li><a href="https://www.lazada.co.id/shop/diarika-beauty-office">Lazada <img
+                                        src="/icon/lazada.png" alt="Diarika Beauty Official Website" width="30px"></a>
                             </li>
-                            <li><a href="https://www.instagram.com/diarika_beauty_official/">Instagram<img src="/icon/instagram.png" alt="Diarika Beauty Official Website" width="30px"></a>
+                            <li><a href="https://www.instagram.com/diarika_beauty_official/">Instagram<img
+                                        src="/icon/instagram.png" alt="Diarika Beauty Official Website"
+                                        width="30px"></a>
                             </li>
                         </ul>
                     </div>
@@ -42,7 +49,9 @@
     <div class="footer-bottom">
         <div class="container pt-0 pb-0">
             <div class="footer-bottom-content">
-                <p class="copyright">© 2022 Diarika Beauty<i class="fa fa-heart"></i></p>
+                <p class="copyright">© 2022 Diarika Beauty<i class="fa fa-heart"></i> Powered by <span
+                        class="underline main-color"><a href="https://primanugraha.com/"
+                            target="_blank">Prima Nugraha</a></span></p>
             </div>
         </div>
     </div>
