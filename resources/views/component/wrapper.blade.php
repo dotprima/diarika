79c97220d0c87e1ea82168c61545f2900a3abd99
Prima Nugraha <dotprima@hotmail.com>
2023-10-14 17:08:42 +0700

    update

--- resources/views/component/wrapper.blade.php
+++ resources/views/component/wrapper.blade.php
@@ -47,8 +47,7 @@
     @livewireStyles
     <!-- Style CSS -->
     <link rel="stylesheet" href="{{ env('APP_URL') }}/assets/css/style.min.css">
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-V38GPWK02H"></script>
+
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-WD3QG2G40C"></script>
     <script>
@@ -61,25 +60,6 @@
 
         gtag('config', 'G-WD3QG2G40C');
     </script>
-
-    <!-- Google Tag Manager -->
-    <script>
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-5QTCDVR');
-    </script>
-    <!-- End Google Tag Manager -->
 </head>
 
 <body>
